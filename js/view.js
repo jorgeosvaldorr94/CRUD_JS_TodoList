1c3d0db5bfbb8e66b25b6dfb200246b0f5a27c4e
Jorge Osvaldo Redonda Rodríguez <jorgeosvaldorr94@gmail.com>
2023-03-31 19:09:57 -0300

    Project Finished

--- js/view.js
+++ js/view.js
@@ -1,5 +1,6 @@
 import AddTodo from "./components/add-todo.js";
 import Modal from "./components/modal.js";
+import Filters from "./components/filters.js";
 
 export default class View {
     constructor() {
@@ -7,12 +8,16 @@ export default class View {
         this.table = document.getElementById('table');
         this.addTodoForm = new AddTodo();
         this.modal = new Modal();
+        this.filters = new Filters();
 
         this.addTodoForm.onClick((title, description) => {
             return this.addTodo(title, description);
         });
 
         this.modal.onClick((id, values) => this.editTodo(id, values));
+
+        // Para el evento Filter
+        this.filters.onClick((filters) => this.filter(filters));
     }
 
     setModel(model) {
@@ -22,6 +27,32 @@ export default class View {
     render() {
         const todos = this.model.getTodos();
         todos.forEach((todo) => this.createRow(todo));
+    }
+
+    filter(filters) {
+        const { type, words } = filters;
+        const [, ...rows] = this.table.getElementsByTagName('tr');
+        for (const row of rows) {
+            const [title, description, completed] = row.children;
+            let shoulHide = false;
+
+            if (words) {
+                shoulHide = !title.innerText.includes(words) && !description.innerText.includes(words);
+            }
+
+            const shouldBeCompleted = type === 'completed';
+            const isCompleted = completed.children[0].checked;
+
+            if (type !== 'all' && shouldBeCompleted !== isCompleted) {
+                shoulHide = true;
+            }
+
+            if (shoulHide) {
+                row.classList.add('d-none');
+            } else {
+                row.classList.remove('d-none');
+            }
+        }
     }
 
     addTodo(title, description) {
@@ -73,7 +104,12 @@ export default class View {
             editBtn.setAttribute('data-toggle', 'modal');
             editBtn.setAttribute('data-target', '#modal')
             editBtn.innerHTML = '<i class="fa fa-pencil"></i>';
-            editBtn.onclick = (e) => this.modal.setValues(todo);
+            editBtn.onclick = (e) => this.modal.setValues({
+                id: todo.id,
+                title: row.children[0].innerText,
+                description: row.children[1].innerText,
+                completed: row.children[2].children[0].checked,
+            });
 
             // Añadirlo al 4to elemento de la tabla
             row.children[3].appendChild(editBtn);
